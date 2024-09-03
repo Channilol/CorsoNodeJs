be4ce6218e6ba2b04fca114be4a3dbf9b10fcfec
Channilol <fcannizzo97@gmail.com>
2024-09-03 09:19:22 +0200

    lezione 13

--- index.js
+++ index.js
@@ -36,6 +36,9 @@ const url = require("url");
 
 //* SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -43,10 +46,12 @@ const server = http.createServer((req, res) => {
     res.end("Overview page");
   } else if (pathName === "/product") {
     res.end("Product page");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
-      "my-own-header": "hello world!",
     });
     res.end("<h1>Page does not exist</h1>");
   }
